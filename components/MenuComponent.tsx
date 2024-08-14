5b3b755b5fbf7ffca9e39571194efb49763fa03c
unknown <althaf.umer@strategic.ae>
2024-08-14 09:37:29 +0400

    menu color updated

--- components/MenuComponent.tsx
+++ components/MenuComponent.tsx
@@ -53,10 +53,10 @@ export default function MenuComponent() {
         <div className="d-flex align-items-end justify-content-between">
           <Link href="/" className="logo-wrapper">
             <img src={`/assets/imgs/AIM-logo.png`} alt="Logo"
-              loading="lazy" className='menu-logo white-logo' />
+               className='menu-logo white-logo' />
 
-            <img src={`/assets/imgs/Aim-black-logo.png`} alt="Logo"
-              loading="lazy" className='menu-logo black-logo' />
+            {/* <img src={`/assets/imgs/Aim-black-logo.png`} alt="Logo"
+              loading="lazy" className='menu-logo black-logo' /> */}
 
           </Link>
 
