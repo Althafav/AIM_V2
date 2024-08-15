f295c9e7a6725bd4d05b419ad8b93d6acd91726e
unknown <althaf.umer@strategic.ae>
2024-08-15 08:51:57 +0400

    accordin fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,10 +14,9 @@ export default function Document() {
         <HeaderComponent />
         <link rel="preconnect" href="https://deliver.kontent.ai" crossOrigin="anonymous" />
         <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-          crossOrigin="anonymous"
-        ></Script>
+          src={`/assets/js/bootstrap.bundle.min.js`}
+          strategy="afterInteractive"
+        />
       </Head>
       <body>
         <Main />
