542d61189d24c21c1f18cda66590f593565fbbc2
unknown <althaf.umer@strategic.ae>
2024-08-16 19:07:28 +0400

    about section :

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { motion } from "framer-motion";
+import Marquee from 'react-fast-marquee';
 
 export default function About() {
     const sentence1 = "AIM Congress is an initiative of the AIM Global Foundation, an independent international organization fully committed to empower the world’s economy by boosting effective promotion strategies and facilitating opportunities for economic productivity and expansion."
     const sentence2 = "AIM Congress has established itself as a leading investment platform in the Middle East, which aspires to ignite positive transformation by creating investment opportunities, upholding solidarity and developing economic relations among nations, as well as addressing the global ordeals that made a profound impact on economic growth."
     const sentence3 = "AIM Congress continues to gather corporate leaders, policy-makers, businessmen, regional and international investors, entrepreneurs, leading academics, and experts showcasing up-to-date information, strategies and knowledge on attracting investments. Which brings together visitors from both the public and private sectors in Abu Dhabi and from all around of the world."
-        
+
     return (
 
         <>
@@ -32,8 +33,15 @@ export default function About() {
                 </div>
             </div>
 
-            <div className='about-us-section-wrapper'>
-                <img src="/assets/imgs/about-section-bg.png" alt="" className='about-us-bg' />
+            <div className='about-us-section-wrapper'
+                style={{
+                    backgroundImage: "url('/assets/imgs/about-section-bg.png')",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                }}
+            >
+               
 
                 <div className="container text-content">
                     <div className="row">
@@ -44,7 +52,7 @@ export default function About() {
                             <p className='' > {sentence2}</p>
                             <p className='' > {sentence3}</p>
 
-                            
+
                         </div>
                     </div>
                 </div>
@@ -74,7 +82,7 @@ export default function About() {
             </div>
 
             <section className="stats-container-wrapper section-container mt-5">
-                <div className="grid-container">
+                <div className="grid-container d-none d-lg-grid">
                     <div className="item1">
                         <img src="/assets/imgs/b2bmembers.png" alt="aim 2025 b2b members count" />
                     </div>
@@ -105,6 +113,39 @@ export default function About() {
                         <img src="/assets/imgs/startups.png" alt="aim 2025" />
                     </div>
 
+                </div>
+                ?
+
+                <div className="d-lg-none">
+                    <Marquee gradient={false}>
+                        <div className="marquee-item">
+                            <img src="/assets/imgs/b2bmembers.png" alt="aim 2025 b2b members count" />
+                        </div>
+                        <div className="marquee-item">
+                            <img src="/assets/imgs/speakers.png" alt="aim 2025 speakers count" />
+                        </div>
+                        <div className="marquee-item">
+                            <img src="/assets/imgs/sessions.png" alt="aim 2025 sessions count" />
+                        </div>
+                        <div className="marquee-item">
+                            <img src="/assets/imgs/exhibitors.png" alt="aim 2025 exhibitors count" />
+                        </div>
+                        <div className="marquee-item">
+                            <img src="/assets/imgs/totalattendence.png" alt="aim 2025 attendence count" />
+                        </div>
+                        <div className="marquee-item">
+                            <img src="/assets/imgs/high-level-dignitades.png" alt="aim 2025" />
+                        </div>
+                        <div className="marquee-item">
+                            <img src="/assets/imgs/total-countries.png" alt="aim 2025" />
+                        </div>
+                        <div className="marquee-item">
+                            <img src="/assets/imgs/highlevelRoundtables.png" alt="aim 2025" />
+                        </div>
+                        <div className="marquee-item">
+                            <img src="/assets/imgs/startups.png" alt="aim 2025" />
+                        </div>
+                    </Marquee>
                 </div>
 
             </section>
