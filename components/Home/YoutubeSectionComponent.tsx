74e744ef9499931fd761353eb0971bb0246b42d9
unknown <althaf.umer@strategic.ae>
2024-08-19 18:59:24 +0400

    updated

--- components/Home/YoutubeSectionComponent.tsx
+++ components/Home/YoutubeSectionComponent.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 export default function YoutubeSectionComponent() {
+    const subHead = "Don't miss out—subscribe to our channel and stay updated "
     return (
         <section className='youtube-section-component'>
             <div className="container">
                 <div className="row">
                     <div className="col-12 d-flex justify-content-center align-items-center flex-column">
-                        <p className='mainHead'>AIM Youtube Videos</p>
-                        <p className='subHead'>Marketing Videos in Youtube will be listed here as well</p>
+                        <p className='mainHead'>AIM Congress on Youtube </p>
+                        <p className='subHead'>{subHead}</p>
                     </div>
 
                     <div className="col-12 d-flex justify-content-center">
