c89fd2d8f6d1b41a594123611a8b8d01853cf23e
unknown <althaf.umer@strategic.ae>
2024-08-19 17:55:45 +0400

    2025 page update4

--- pages/event-highlights.tsx
+++ pages/event-highlights.tsx
@@ -3,10 +3,11 @@ import { BenefitsOfParticipatingInAim2025 } from '@/contants/aimbenefits'
 
 import Link from 'next/link'
 import React from 'react'
+import Marquee from 'react-fast-marquee'
 
 export default function EventHighlights() {
     const about2025 = "The Annual Investments Meeting (AIM) 2025 is dedicated to advancing the future of globalized investment and its industrial impact by charting the course for emerging investment trends. AIM 2025 aims to create a more balanced global framework, identify opportunities within a dynamic economic environment, and unlock the potential of every business, country, and region. By fostering economic diversification and providing actionable insights into emerging trends, AIM 2025 will empower investors to make strategic, informed decisions that align with their goals and requirements as well as equip participants to navigate a rapidly evolving landscape, ensuring that their investment choices are both prudent and aligned with long-term objectives. "
-    
+
     const aboutAbuDhabi = "Where the allure of tradition meets the pulse of innovation. As we prepare to host AIM 2025, immerse yourself in a city where ancient culture seamlessly intertwines with cutting-edge advancements. Envision yourself exploring vibrant souks and contemporary marvels, where every corner unveils a new experience. From the serene beauty of the Corniche to the dynamic energy of cutting-edge developments, Abu Dhabi transcends being merely a destination—it's an odyssey of discovery and inspiration. Join us in experiencing a city that embodies both timeless elegance and forward-thinking vision, setting the stage for an unforgettable AIM 2025. "
     return (
         <div className='event-highlights-page-wrapper'>
@@ -102,6 +103,26 @@ export default function EventHighlights() {
                             </div>
                         ))}
                     </div>
+                </div>
+            </section>
+
+            <section >
+                <Marquee>
+
+                    <p className='stay-tuned'>Stay tuned—our AGENDA is coming, packed with fresh perspectives and groundbreaking ideas! </p>
+
+                </Marquee>
+            </section>
+
+            <section>
+                <div className='container'>
+                    <div className="row">
+                        <div className="col-12 d-flex justify-content-center flex-column gap-2 align-items-center">
+                            <p className='join-us-caption'>Join us at AIM Congress 2025</p>
+                            <p className='join-us-caption-2'>Building a better tomorrow, today!</p>
+                        </div>
+                    </div>
+
                 </div>
             </section>
 
