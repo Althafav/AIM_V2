64d796b37401e0a3cb0d8c9290d81ba7c22d2ff8
unknown <althaf.umer@strategic.ae>
2024-08-21 19:34:34 +0400

    stats section updated

--- components/BannerComponent.tsx
+++ components/BannerComponent.tsx
@@ -8,77 +8,71 @@ export default function BannerComponent() {
 
     const customHeading = "Annual Investment Meeting"
     return (
-        <div className="banner-wrapper">
-            <div className="video-section">
+        <>
 
-                <video width="100%" autoPlay loop muted controls={false} preload="auto">
-                    <source src="https://d2g6bqkf4g3jqe.cloudfront.net/videos/aim-bg.mp4" type="video/mp4" className='banner-video' width="100%" />
-                </video>
-            </div>
+            <div className="banner-wrapper">
+                <div className="video-section">
 
-            <div className="text-container container">
-                <h1 className='banner-heading'>
-                   {customHeading}
-                </h1>
-                <h2 className='banner-heading-2'>Harnessing new potential around the world!</h2>
-             
+                    <video width="100%" autoPlay loop muted controls={false} preload="auto">
+                        <source src="https://d2g6bqkf4g3jqe.cloudfront.net/videos/aim-bg.mp4" type="video/mp4" className='banner-video' width="100%" />
+                    </video>
+                </div>
 
-                <div className='mt-3 d-flex align-items-lg-center align-items-start gap-3 flex-lg-row flex-column-reverse'>
-                    <button className='register-interest-cta mb-4 mb-lg-0'>Register your interest</button>
-                    <p className='date-venue'>7 - 9 April | Abu dhabi, United Arab Emirates</p>
+                <div className="text-container container">
+                    <h1 className='banner-heading'>
+                        {customHeading}
+                    </h1>
+                    <h2 className='banner-heading-2'>Harnessing new potential around the world!</h2>
+
+
+                    <div className='mt-3 d-flex align-items-lg-center align-items-start gap-3 flex-lg-row flex-column-reverse'>
+                        <button className='register-interest-cta mb-4 mb-lg-0'>Register your interest</button>
+                        <p className='date-venue'>7 - 9 April | Abu dhabi, United Arab Emirates</p>
+                    </div>
+
                 </div>
 
 
 
-           
-
-
 
 
 
             </div>
+
+
 
             <div className="stats-content ">
-               
-
-                <ul className='row g-lg-5 g-1'>
-                    <li className='col-lg-3 col-6 mb-3 mb-lg-0'>
-                        <div className='d-flex flex-column gap-0'>
-
-                            <span className='count'>179</span>
-                            <span>Countries</span>
-                        </div>
-                    </li>
-
-                    <li className='col-lg-3 col-6 mb-3 mb-lg-0'>
-                        <div className='d-flex flex-column gap-0'>
-
-                            <span className='count'>12,427</span>
-                            <span>Participants</span>
-                        </div>
-                    </li>
-
-                    <li className='col-lg-3 col-6 mb-3 mb-lg-0'>
-                        <div className='d-flex flex-column gap-0'>
-
-                            <span className='count'>412</span>
-                            <span>Sessions</span>
-                        </div>
-                    </li>
-
-                    <li className='col-lg-3 col-6 mb-3 mb-lg-0'>
-                        <div className='d-flex flex-column gap-0'>
-
-                            <span className='count'>927</span>
-                            <span>Speakers</span>
-                        </div>
-                    </li>
-                </ul>
+                <div className="container ">
+                    <ul className="row g-4">
+                        <li className="col-lg-3 col-6 ">
+                            <div className="d-flex flex-column align-items-lg-center">
+                                <span className="count">179</span>
+                                <span>Countries</span>
+                            </div>
+                        </li>
+                        <li className="col-lg-3 col-6 ">
+                            <div className="d-flex flex-column align-items-lg-center">
+                                <span className="count">12,427</span>
+                                <span>Participants</span>
+                            </div>
+                        </li>
+                        <li className="col-lg-3 col-6 ">
+                            <div className="d-flex flex-column align-items-lg-center">
+                                <span className="count">412</span>
+                                <span>Sessions</span>
+                            </div>
+                        </li>
+                        <li className="col-lg-3 col-6 ">
+                            <div className="d-flex flex-column align-items-lg-center">
+                                <span className="count">927</span>
+                                <span>Speakers</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
 
 
-
-
-        </div>
+        </>
     )
 }
