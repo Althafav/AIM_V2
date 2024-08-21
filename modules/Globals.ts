e995729cd480aac724771cee12abcd49f136dbe9
unknown <althaf.umer@strategic.ae>
2024-08-21 08:17:19 +0400

    updated

--- modules/Globals.ts
+++ modules/Globals.ts
@@ -4,10 +4,11 @@ import { TypeResolver } from "./TypeResolvers";
 const KontentDelivery = require("@kentico/kontent-delivery");
 
 export default class Globals {
-    static PROJECT_ID: string = "e3428e80-3966-0085-ee98-3bee1120f101";
+static PROJECT_ID: string = "615577b9-4e2b-0074-b4f8-20f11d30f5ae";
 
-    static SECURE_API_KEY: string =
-        "ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.ew0KICAianRpIjogIjcxYjA2OTY0ODE5NTRmNjI4YjA5ZGQ2NzZjNmYwMWJiIiwNCiAgImlhdCI6ICIxNjc2NTI3MTUxIiwNCiAgImV4cCI6ICIxNzA4MDYzMTQwIiwNCiAgInZlciI6ICIxLjAuMCIsDQogICJwcm9qZWN0X2lkIjogImUzNDI4ZTgwMzk2NjAwODVlZTk4M2JlZTExMjBmMTAxIiwNCiAgImF1ZCI6ICJkZWxpdmVyLmtlbnRpY29jbG91ZC5jb20iDQp9.pwiythDp7iXJLTbOwC868UnS0j8wj1DwWyYUViFBLXI";
+  static SECURE_API_KEY: string =
+    "ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.ew0KICAianRpIjogIjE5YjlkNjllZGMwZTQyN2U5NjNmZDlhNTE1MzRmODlhIiwNCiAgImlhdCI6ICIxNjI2MzMyMDY0IiwNCiAgImV4cCI6ICIxOTcxOTMyMDY0IiwNCiAgInZlciI6ICIxLjAuMCIsDQogICJwcm9qZWN0X2lkIjogIjYxNTU3N2I5NGUyYjAwNzRiNGY4MjBmMTFkMzBmNWFlIiwNCiAgImF1ZCI6ICJkZWxpdmVyLmtlbnRpY29jbG91ZC5jb20iDQp9.QFwPat7Wia81kJxmckCHhS9vcJLg6T5mPdHxFUnlr18";
+
 
     static KontentClient: any = new KontentDelivery.DeliveryClient({
         projectId: Globals.PROJECT_ID,
@@ -21,6 +22,8 @@ export default class Globals {
 
     static SITE_NAME = "AIM";
     static CURRENT_LANG_CODENAME: string = "default";
+
+    static BASE_URL_AIM_Website: string = "http://localhost:4000";
 
     static BASE_URL: string =
         process.env.NODE_ENV === "production"
--- pages/articles.tsx
+++ pages/articles.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { motion, useInView } from "framer-motion";
 
 import Globals from '@/modules/Globals';
@@ -21,42 +21,24 @@ const cardVariants = {
     visible: { y: 0, opacity: 1, transition: { duration: 0.6 } },
 };
 
-export async function getStaticProps() {
-    try {
-        const datasourceStr: string = await Globals.KontentClient.item("blog_page")
-            .languageParameter(Globals.CURRENT_LANG_CODENAME)
-            .toObservable()
-            .toPromise()
-            .then((r: any) => {
-                return JSON.stringify(r.item);
-            });
 
-        const data: Blogs = JSON.parse(datasourceStr);
-        console.log(data, "this is data")
-        return {
-            props: {
-                data,
-            },
-            revalidate: 120,
-        };
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        return {
-            props: {
-                data: null,
-            },
-        };
-    }
-}
-
-
-function ListPage({ data }: { data: Blogs }) {
-    if (!data) {
-        return <SpinnerComponent />;
-    }
-    var formatName = "";
+export default function Articles() {
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true });
+    const [pageData, setPageData] = useState<Blogs | null>(null);
+
+    useEffect(() => {
+        Globals.KontentClient.item("blog_page")
+            .toObservable()
+            .subscribe((response: any) => {
+                console.log('API Response:', response);
+                setPageData(response.item);
+            });
+    }, []);
+
+    if (!pageData) {
+        return <SpinnerComponent />;
+    }
     return (
         <div className='articles-page-wrapper'>
             <div className="inner-banner-section-wrapper">
@@ -79,43 +61,30 @@ function ListPage({ data }: { data: Blogs }) {
                         initial="hidden"
                         animate={isInView ? "visible" : "hidden"}
                     >
-                        {/* {BlogsData.map((item: any, index: number) => {
+
+
+                        {pageData.articlesItems.value.map((m: any, index: number) => {
+
+                            var item: Blogitems = m;
                             return (
-                                <motion.div className="col-lg-3 mb-3" key={`article-${index}`}
-                                    variants={cardVariants}
+                                <div className="col-lg-3 mb-3" key={`article-${index}`}
+
                                 >
+                                    <div className='blog-card' 
+                                        
+                                    >
 
-                                    <div className='blog-card'>
-
-                                        <img src={item.image} alt="" className='' />
-                                        <p className='name'>{item.name}</p>
+                                        <img src={item.image.value[0].url} alt="" className='' />
+                                        <p className='name'>{item.heading.value}</p>
                                     </div>
-                                </motion.div>
+                                </div>
                             )
-                        })} */}
+                        })}
 
-                        {data.articlesItems.value.map((i: any, key: number) =>
-                            i.chooseArticle.value == null
-                                ? ((formatName = i.system.id),
-                                    (
-                                        <motion.div className="col-lg-3 mb-3" key={`article-${key}`}
-                                            variants={cardVariants}
-                                        >
 
-                                            <div className='blog-card'>
-
-                                                <img src={i.image.value[0].url} alt="" className='' />
-                                                <p className='name'>{i.heading.value}</p>
-                                            </div>
-                                        </motion.div>
-                                    ))
-                                : ""
-                        )}
                     </motion.div>
                 </div>
             </section>
         </div>
     )
 }
-
-export default ListPage;
