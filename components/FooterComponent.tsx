9ee682bddf89a6b81e3313a34fb8bd7e1c1fc69e
unknown <althaf.umer@strategic.ae>
2024-08-26 09:40:25 +0400

    updated

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -117,7 +117,7 @@ export default function FooterComponent() {
 
                   <Link href="/startup">
 
-                    <p>Startups Unicorn</p>
+                    <p>Startups & Unicorn</p>
                   </Link>
 
                   <Link href="/entrepreneurs">
--- components/Home/SustainbleSecComponent.tsx
+++ components/Home/SustainbleSecComponent.tsx
@@ -11,13 +11,13 @@ export default function SustainbleSecComponent() {
 
             <div className="section-container">
                 <div className="row g-2">
-                    <div className="col-md-4 ">
+                    <div className="col-lg-4 ">
                         <div className="left-section">
                             <h1 className='section-heading--sustainable'>
                                 AIM Sustainable Development</h1>
                         </div>
                     </div>
-                    <div className="col-md-8 ">
+                    <div className="col-lg-8 ">
                         <div className="right-section">
                             <p className='paragraph'>The Annual Investment Meeting (AIM), one of the world’s foremost investment platforms, is deeply committed to harnessing Corporate Social Responsibility (CSR) to drive substantial positive outcomes in climate action and sustainable development. Spanning from March 20, 2020, to March 19, 2025, AIM Congress has been at the forefront of implementing transformative initiatives that align with societal values and promote environmental stewardship. One of the standout projects under this commitment focuses on providing safe water to hundreds of households in the Central Province of Zambia.</p>
                             <img src="/assets/imgs/solar.png" alt="" className='mt-3 mb-3 w-100 cover' />
