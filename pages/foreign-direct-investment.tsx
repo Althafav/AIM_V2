05f35b2aa275b180e63af63be530e29a1300191a
unknown <althaf.umer@strategic.ae>
2024-10-03 18:23:50 +0400

    updated fdi4

--- pages/foreign-direct-investment.tsx
+++ pages/foreign-direct-investment.tsx
@@ -63,7 +63,7 @@ export default function ForiegnDirectInvestment() {
 
                             <div>
                                 <Link href="/register-interest/fdi">
-                                
+
                                 </Link>
                                 <button className='cta-btn fdi text-white'>Register your interest</button>
                             </div>
@@ -90,7 +90,55 @@ export default function ForiegnDirectInvestment() {
             {/* <AboutComponentRevamp pageData={pageData} /> */}
             <FeaturesActivities pageData={pageData} />
             <ParticipateSection pageData={pageData} />
-            <KeyAgenda pageData={pageData} />
+            {/* <KeyAgenda pageData={pageData} /> */}
+            <motion.div
+                className='key-agenda-section-wrapper'
+                style={{
+                    backgroundImage: `url(${pageData.keyagendabackgroundimage.value[0]?.url})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                }}
+
+            >
+                <div className="container">
+
+
+                    <div className="row mt-5 g-3 justify-content-center">
+                        <div className="col-lg-4 d-flex justify-content-center align-items-center">
+                            <motion.h2
+                                initial={{ y: 100, opacity: 0 }}
+                                whileInView={{ y: 0, opacity: 1 }}
+                                viewport={{ once: true, amount: 0.1 }}
+                                transition={{ duration: 0.8, delay: 0.2 }}
+                                className='text-center section-heading text-white fw-normal'>{pageData.keyagendaheading.value}</motion.h2>
+                        </div>
+                        <div className="col-lg-8">
+                            <div className="row g-3">
+                                {pageData.keyagendaitems.value.map((m: any, index) => {
+                                    var item: Serviceitem = m;
+                                    return (
+                                        <motion.div
+                                            className="col-lg-3 col-md-4 col-6"
+                                            key={index}
+
+                                        >
+                                            <motion.div className="key-agenda-card" initial={{ y: 100, opacity: 0 }}
+                                                whileInView={{ y: 0, opacity: 1 }}
+                                                transition={{ duration: 0.8, delay: 0.2 * index, ease: 'easeInOut' }}
+                                                viewport={{ once: true, amount: 0.1 }}>
+                                                <h4 className="name">{item.name.value}</h4>
+                                            </motion.div>
+                                        </motion.div>
+                                    )
+                                })}
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </motion.div>
+
             <section className='frequently-asked-questions-wrapper'>
                 <AccordionComponent pageData={pageData} />
             </section>
