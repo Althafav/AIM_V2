ccccf7ca01c782516c5c82e511a8f949be72e064
unknown <althaf.umer@strategic.ae>
2024-10-09 14:07:16 +0400

    ff key players

--- components/Portfolio/future-finance/KeyPlayersCarousel.tsx
+++ components/Portfolio/future-finance/KeyPlayersCarousel.tsx
@@ -15,32 +15,37 @@ const KeyPlayersCarousel: React.FC<PageDataProps> = ({ pageData }) => {
     }
 
     return (
-        <div className='partner-carousel-wrapper'>
+        <div className='keyplayers-carousel-wrapper'>
             <div className="container">
                 <div className="row">
                     <div className="col-12">
                         <h2 className='section-heading text-center'> {pageData.keyplayersheading.value}</h2>
                     </div>
                 </div>
-            </div>
-            <div className='portfolio-speaker-carousel owl-carousel mt-3'>
-                {pageData.keyplayersitems.value.map((m: any, index: number) => {
-                    var item: Partnerspageitem = m;
-                    return (
-                        <div className='key-players-card' key={`keyplayers-${index}`}>
-                            <div className="card-speaker-item" >
-                                <img width={175} height={175} src={item.logo?.value[0]?.url} alt={item.name.value}
-                                 style={{objectFit: "contain", marginRight: "10px"}}/>
+
+                <div className='keyplayers-carousel owl-carousel mt-lg-5 mt-3'>
+                    {pageData.keyplayersitems.value.map((m: any, index: number) => {
+                        var item: Partnerspageitem = m;
+                        return (
+                            <div className='key-players-wrap' key={`keyplayers-${index}`}>
+                                <div className="keyplayers-card">
+
+                                    <img width={175} height={175} src={item.logo?.value[0]?.url} alt={item.name.value}
+                                        style={{ objectFit: "contain", marginRight: "10px" }} className='key-player-image' />
+                                </div>
+
 
                                 <div className="card-body-speaker">
-                                    <p className="name text-dark" >{item.name.value}</p>
+                                    <p className="key-player-name text-dark" >{item.name.value}</p>
 
                                 </div>
+
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
             </div>
+
         </div>
     )
 }
--- public/assets/js/speakerCarousel.js
+++ public/assets/js/speakerCarousel.js
@@ -61,3 +61,35 @@ $(".portfolio-speaker-carousel").owlCarousel({
     },
   },
 });
+
+
+$(".keyplayers-carousel").owlCarousel({
+  autoplayTimeout: 5000, // 5 seconds autoplay timeout
+  smartSpeed: 1000, // Transition speed for smooth scrolling
+  autoplaySpeed: 1000,
+  margin: 40,
+  autoplay: true,
+  items: 4,
+  loop: true,
+  dots: false,
+  nav: false,
+  navText: [
+    "<i class='fa fa-angle-left' aria-hidden='true'></i>",
+    "<i class='fa fa-angle-right' aria-hidden='true'></i>",
+  ],
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+    1200: {
+      items: 4,
+    },
+  },
+});
